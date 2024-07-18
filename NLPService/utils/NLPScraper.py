ad7a66b98cb19844083805c375a2f13547e66266
albayona <al.bayona@uniandes.edu.co>
2024-07-18 03:37:53 -0500

    NA

--- NLPService/utils/NLPScraper.py
+++ NLPService/utils/NLPScraper.py
@@ -85,7 +85,7 @@ def login():
 
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    # chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--no-sandbox')
 
     chrome_options.add_argument(f"--user-data-dir=login_data/original_data")
 
