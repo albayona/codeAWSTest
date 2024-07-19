c0c8a98bb5ce9b6b4bfe34c157c4fe632020373d
albayona <al.bayona@uniandes.edu.co>
2024-07-19 07:09:07 -0500

    Scraper serrvice

--- ScraperService/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Post(models.Model):
-    link = models.CharField(max_length=200, unique=True)
-
-    def __str__(self):
-        return self.link
--- ScraperService/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-# import everything from views
-from .views import *
-
-urlpatterns =[
-    path('scrape/', scrape_posts_view, name='scrape'),
-
-]
