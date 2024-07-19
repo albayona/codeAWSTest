a3c7ef7727faa25211a42e288966cc290758f4b8
albayona <al.bayona@uniandes.edu.co>
2024-07-18 22:40:31 -0500

    NA

--- ScraperService/api/utils/scraper.py
+++ ScraperService/api/utils/scraper.py
@@ -103,6 +103,8 @@ class Scraper:
         for simple_post in simple_posts:
             if non_existent(simple_post):
                 self.request_NLP_scrape(simple_post)
+                sleep(1)
+
 
     def request_NLP_scrape(self, simple_post):
         post = create_post(simple_post)
